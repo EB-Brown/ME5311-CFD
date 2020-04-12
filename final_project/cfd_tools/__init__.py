58709f0884adddd39d888f8d2e1c91f6eadf90ac
eb <mullen201079@yahoo.com>
2020-04-12 19:50:14 -0400

    Refactored the `poisson_pressure` module to `calculus`

--- final_project/cfd_tools/__init__.py
+++ final_project/cfd_tools/__init__.py
@@ -6,11 +6,14 @@ __all__ = [
     "plot_contour",
     "LinearMesh",
     "plot_convergence",
+    "Field",
+    "FluidProfile",
 ]
 
-from .poisson_pressure import get_dct2_solution
+from .calculus import get_dct2_solution
 from .convergence import p_norm, infinity_norm, get_convergence_fit
 from .plots import plot_contour, plot_convergence
 from .mesh import LinearMesh
+from .simulations import FluidProfile, Field
 
 __version__ = 1.0
