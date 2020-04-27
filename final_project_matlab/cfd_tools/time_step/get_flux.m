9a26457a282eaf1f3f34fbf0716a553690d24676
eb <mullen201079@yahoo.com>
2020-04-26 21:05:58 -0400

    Calibrated ghost points

--- final_project_matlab/cfd_tools/time_step/get_flux.m
+++ final_project_matlab/cfd_tools/time_step/get_flux.m
@@ -5,38 +5,42 @@ function [uu_flux, vu_flux, uv_flux, vv_flux] = get_flux( ...
 % ut_flux, vt_flux
 
 % Trim off the ghost points
-u = u_vel(ghost:x_num + ghost - 1, ghost:y_num + ghost - 2); % M X N-1
-v = v_vel(ghost:x_num + ghost - 2, ghost:y_num + ghost - 1); % M-1 X N
+u = u_vel(ghost:x_num + ghost, ghost + 1:y_num + ghost); % M X N-1
+v = v_vel(ghost + 1:x_num + ghost, ghost:y_num + ghost); % M-1 X N
 
-ux_neg_2 = x_num - 1; % Index of second to last column in the U domain
-vx_neg_1 = ux_neg_2; % Index of last column in V domain
-vx_neg_2 = vx_neg_1 - 1; % Index of second to last column in the V domain
+% x_num: M-1
+% y_num: N-1
 
-vy_neg_2 = y_num - 1; % Index of second to last row in the V domain
-uy_neg_1 = vy_neg_2; % Index of last row in U domain
-uy_neg_2 = uy_neg_1 - 1; % Index of second to last row in the U domain
+u_right = x_num + 1;
+v_right = x_num;
+
+u_top = y_num; % Index of last row in U domain
+v_top = y_num + 1;
+
+uy_neg_2 = u_top - 1; % Index of second to last row in the U domain
+vx_neg_2 = v_right - 1; % Index of second to last column in the V domain
 
 % M-1 X N-1 -> dx -> M-2 X N-1
-ux_shift = (u(2:x_num, :) + u(1:ux_neg_2, :)) / 2;
+ux_shift = (u(2:u_right, :) + u(1:x_num, :)) / 2;
 
-% M-2 X N -> dy -> M-2 X N-1
-vx_shift = zeros(x_num, y_num);
-vx_shift(2:vx_neg_1, :) = (v(2:vx_neg_1, :) + v(1:vx_neg_2, :)) / 2;
+% Multi used for vu and uv flux. Kept as M X N and indexed on appropriate index
+vx_shift = zeros(u_right, v_top);
+vx_shift(2:x_num, :) = (v(2:v_right, :) + v(1:vx_neg_2, :)) / 2;
 
-% M-2 X N -> dy -> M-2 X N-1
-uy_shift = zeros(x_num, y_num);
-uy_shift(:, 2:uy_neg_1) = (u(:, 2:uy_neg_1) + u(:, 1:uy_neg_2)) / 2;
+% Multi used for vu and uv flux. Kept as M X N and indexed on appropriate index
+uy_shift = zeros(u_right, v_top);
+uy_shift(:, 2:y_num) = (u(:, 2:u_top) + u(:, 1:uy_neg_2)) / 2;
 
-vy_shift = (v(:, 2:y_num) + v(:, 1:vy_neg_2)) / 2;
+vy_shift = (v(:, 2:v_top) + v(:, 1:y_num)) / 2;
 
 uv = uy_shift.* vx_shift; % M X N
 
 % U Flux
 uu_flux = ux_shift.^ 2; % M-1 X N-1 -> dx -> M-2 X N-1
-vu_flux = uv(2:ux_neg_2, :); % M-2 X N -> dy -> M-2 X N-1
+vu_flux = uv(2:x_num, :); % M-2 X N -> dy -> M-2 X N-1
 
 % V Flux
-uv_flux = uv(:, 2:vy_neg_2); % M X N-2 -> dx -> M-1 X N-2
+uv_flux = uv(:, 2:y_num); % M X N-2 -> dx -> M-1 X N-2
 vv_flux = vy_shift.^ 2; % M-1 X N-1 -> dy -> M-1 X N-2
 
 end
