1184a3179e6e1771ae31ff882a69166eeaf7ed2c
eb <mullen201079@yahoo.com>
2020-04-27 22:05:45 -0400

    Refactored `boundaries` into `velocity_boundaries`

--- final_project_matlab/cfd_tools/fluid_profile/boundaries.m
+++
@@ -1,9 +0,0 @@
-function [u_vel, v_vel] = boundaries(x_num, y_num, ghost, u_vel, v_vel)
-
-    u_vel(ghost,:)=0; % Left - no penetration
-    u_vel(x_num+ghost,:)=0; % right - no penetration
-
-    v_vel(:,ghost)=0; % Bottom wall - no penetration
-    v_vel(:,y_num+ghost)=0; % Top wall - no penetration
-
-end
