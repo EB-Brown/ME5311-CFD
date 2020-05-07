a0021bdb60f6fcc8092f427b49d24943e044cf42
eb <mullen201079@yahoo.com>
2020-05-07 09:17:39 -0400

    Added diffusion CFL for dt calculations

--- final_project_matlab/cfd_tools/simulate.m
+++ final_project_matlab/cfd_tools/simulate.m
@@ -7,7 +7,17 @@ function [u_vel, v_vel, theta, time] = simulate( ...
 )
 
 if cfl % CFL is defined, use dynamic dt
-    dt = cfl / (max(max(abs(u_vel)))/dx + max(max(abs(v_vel)))/dy);
+    max_u = max(max(abs(u_vel)));
+    max_v = max(max(abs(v_vel)));
+    if (max_u == 0) && (max_v == 0)
+        max_u = 1;
+        max_v = 1;
+    end
+
+    dt_convection = cfl / (max_u/dx + max_v/dy);
+    dt_diffusion = cfl / (prandtl * (1/dx^2 + 1/dy^2));
+    dt = min(dt_convection, dt_diffusion);
+
 end
 
 time = start_time;
@@ -27,7 +37,7 @@ while true
     end
 
     if cfl % CFL is defined, use dynamic dt
-        dt = get_next_dt(cfl, u_vel, v_vel, dx, dy, dt);
+        dt = get_next_dt(cfl, u_vel, v_vel, prandtl, dx, dy, dt);
     end
 
 end
--- final_project_matlab/cfd_tools/time_step/get_next_dt.m
+++ final_project_matlab/cfd_tools/time_step/get_next_dt.m
@@ -1,6 +1,16 @@
-function dt = get_next_dt(cfl_target, u_vel, v_vel, dx, dy, dt)
+function dt = get_next_dt(cfl_target, u_vel, v_vel, prandtl, dx, dy, dt)
 
-cfl_max = dt * (max(max(abs(u_vel))) / dx + max(max(abs(v_vel))) / dy) ;
+max_u = max(max(abs(u_vel))) ;
+max_v = max(max(abs(v_vel))) ;
+if (max_u == 0) && (max_v == 0)
+        max_u = 1;
+        max_v = 1;
+end
+
+cfl_convection = dt * (max_u / dx + max_v / dy) ;
+cfl_diffusion = prandtl * dt * (1 / dx ^ 2 + 1 / dy ^ 2);
+cfl_max = max(cfl_diffusion, cfl_convection);
+
 dt = 0.5 * dt * (cfl_target / cfl_max + 1);
 
 end
