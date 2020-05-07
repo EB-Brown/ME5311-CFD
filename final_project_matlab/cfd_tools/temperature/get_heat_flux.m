d619a256228330ca938e64bef180f314678eec43
eb <mullen201079@yahoo.com>
2020-05-07 16:14:33 -0400

    Added function for calculating convective heat flux along the left and right walls

---
+++ final_project_matlab/cfd_tools/temperature/get_heat_flux.m
@@ -0,0 +1,12 @@
+function [left_flux, right_flux] = get_heat_flux( ...
+    temperature, left_temp, right_temp, dx, x_num, y_num, ghost ...
+)
+
+theta = temperature(gp1:ghost+x_num, gp1:ghost + y_num);
+
+left_flux = sum(temperature(1,:) - left_temp) / y_num;
+
+along_right = temperature(ghost,gp1:ghost + y_num);
+right_flux = sum(right_temp - temperature(x_num,:)) / y_num;
+
+end
