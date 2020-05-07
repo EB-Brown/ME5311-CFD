5d3bda09a5df893f8169d5e4b45f8837c981dc81
eb <mullen201079@yahoo.com>
2020-05-07 12:44:17 -0400

    Added documentation and reduced CFL for simulation stability

--- final_project_matlab/simulation_simple.m
+++ final_project_matlab/simulation_simple.m
@@ -1,20 +1,47 @@
 clear all
 clc
 
+%{
+This simulation initializes the temperature profile with a constant value of 0.5
+%}
+
 %%%%%%%%%%%%%%%%% Inputs %%%%%%%%%%%%%%%%%
+
+%{
+Modify the inputs of this section to control the simulation properties
+
+PARAMETERS
+x_len: the length of the domain in the x direction
+y_len: the length of the domain in the y direction
+end_time: the amount of time the simulation will run for
+left_wall_temperature: The left wall temperature condition. An array can be
+    used as long as it represents a continous function and its length is
+    `y_len + 2*ghost`.
+cfl_target: The target CFL for controlling the time step size
+x_num: Number of grid cells in the x direction
+y_num: Number of grid cells in the y direction
+ghost: Number of ghost cells
+prandtl: The Prandtl number for the simulation
+rayleigh: The Rayleigh number for the simulation (Requires adjusting
+    `cfl_target` accordingly to assure stability)
+number_of_plots: The number of plots you would like to generate and save. A
+    minimum of 1 is required to cover the initial condition. The time domain is
+    divided into `end_time / (number_of_plots - 1)` chunks. A plot is generated
+    after each chunk has been simulated.
+%}
 
 % physical domain
 x_len = 0.5;
 y_len = 2;
-end_time = 0.015;
+end_time = 0.01;
 left_wall_temperature = 1; % const or func where 0 <= left_wall_temperature <= 1
 
 % Dynamic time steps are calculated using the relaxation method
-cfl_target = 0.7;
+cfl_target = 0.5;
 
 % Number of grid cells
-x_num = 64;
-y_num = 256;
+x_num = 50;
+y_num = 200;
 ghost = 1; % number  of ghost cells
 
 % Simulation Parameters
@@ -23,7 +50,7 @@ rayleigh = 1e6; % Rayleigh number controls convective driver
 
 % Number of plots generated in equal time intervals.
 % Initial and final profiles are included
-number_of_plots = 11;
+number_of_plots = 101;
 
 %%%%%%%%%%%%%%%%% Setup %%%%%%%%%%%%%%%%%
 
